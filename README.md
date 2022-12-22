104662b1908ff6e12c8e1956249fc68dc6c16756
gaudhiwaa <gaudhiwaa@gmail.com>
2022-12-22 19:47:42 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# PWEB-Laporan-PDF
+
+- Nama : Gaudhiwaa Hendrasto
+- NRP : 5025201066
+- Kelas : Pemrograman Web C
+- Tahun : 2022/2023
---
+++ tutorial.sql
@@ -0,0 +1,50 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 22, 2022 at 06:57 AM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tutorial`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mahasiswa`
+--
+
+CREATE TABLE `mahasiswa` (
+  `nim` char(8) DEFAULT NULL,
+  `nama_lengkap` varchar(100) DEFAULT NULL,
+  `no_hp` varchar(15) DEFAULT NULL,
+  `tanggal_lahir` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`nim`, `nama_lengkap`, `no_hp`, `tanggal_lahir`) VALUES
+('5025201066', 'GAUDHIWAA HENDRASTO', '081234567890', '2002-07-02'),
+('5025201067', 'M NAUFAL', '081234567890', '2003-07-08'),
+('5025201068', 'M TAUFIQ', '081234567890', '2001-02-04'),
+('5025201069', 'M RAHMAN', '081234567890', '2000-01-02');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
